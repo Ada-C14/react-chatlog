5141ec5b7039de5e768b38908262472f9a3f38e8
Tram Bui <pbui17@cmc.edu>
2020-12-20 09:24:18 -0800

    added the ChatLog components to the App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   console.log(chatMessages);
@@ -15,7 +16,7 @@ const App = () => {
       </header>
       <main>
         <ChatEntry sender={singleChat.sender} body={singleChat.body} timeStamp={singleChat.timeStamp} />
-        
+        <ChatLog chatMessages={chatMessages} />
       </main>
     </div>
   );
