5e64ffb07e95f19c94f25d22d22d99359f2d5efe
Tram Bui <pbui17@cmc.edu>
2020-12-20 09:05:02 -0800

    added chatentry to the app and rendered it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   console.log(chatMessages);
 
+  const singleChat = chatMessages[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Secrets between {chatMessages[0].sender} and {chatMessages[1].sender} </h1>
       </header>
       <main>
-
+        <ChatEntry sender={singleChat.sender} body={singleChat.body} timeStamp={singleChat.timeStamp} />
+        
       </main>
     </div>
   );
