426dc3812d7fcb8f2252f17a21cf2ec2b167ede5
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-22 21:22:08 -0800

    added a single chat msg to App.js and passed the tests for wave1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -8,10 +9,13 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-
+        <ChatEntry 
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timeStamp="2018-05-29T22:49:06+00:00" />
       </main>
     </div>
   );
